79fd9bd19355d56a1b1c1e089e1fa9fcff66ab58
Sunchit Anand <sunchit.anand@gmail.com>
2020-04-01 00:46:00 -0700

    fix nearby checkpoint

---
+++ Dexter/Models/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  
+//
+//  Created by Sunchit Anand on 2/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
